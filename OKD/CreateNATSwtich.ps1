18e7512e4265a6da8faa388dca8d163a6b59f61d
BeOnPositiveAttitude <83788467+BeOnPositiveAttitude@users.noreply.github.com>
2025-05-03 09:08:05 +0400

    Update CreateNATSwtich.ps1

--- OKD/CreateNATSwtich.ps1
+++ OKD/CreateNATSwtich.ps1
@@ -1,14 +1,17 @@
 $natSwitchName = "NATSwitch"
-$natNetwork = "192.168.200.0"
 $natRouterAddress = "192.168.200.1"
+$natNetwork = "192.168.200.0"
 $natPrefixLength = "24"
-
-New-VMSwitch -SwitchName $natSwitchName -SwitchType Internal -Verbose
-
-$natSwitch = Get-NetAdapter | where {(($_.name -like ("vEthernet ($natSwitchName)")))}
-
-New-NetIPAddress $natRouterAddress -PrefixLength $natPrefixLength -InterfaceIndex $natSwitch.interfaceindex -Verbose
-
 $natNetworkFull = $natNetwork + "/" + $natPrefixLength
 
+# Создаем новый коммутатор в Hyper-V (и соответственно новый сетевой адаптер в ОС)
+New-VMSwitch -SwitchName $natSwitchName -SwitchType Internal -Verbose
+
+# Получаем все параметры созданного сетевого адаптера и сохраняем их в переменную
+$natSwitch = Get-NetAdapter | where {(($_.name -like ("vEthernet ($natSwitchName)")))}
+
+# Вешаем на наш новый интерфейс IP-адрес
+New-NetIPAddress $natRouterAddress -PrefixLength $natPrefixLength -InterfaceIndex $natSwitch.interfaceindex -Verbose
+
+# Создаем новую NAT-сеть
 New-NetNat -Name HyperV-NatNetwork -InternalIPInterfaceAddressPrefix $natNetworkFull -Verbose
